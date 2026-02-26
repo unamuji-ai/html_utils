25b2063ca91a0d5ec2e538a4e87005617daca43c
koji-hasunuma <koji-hasunuma@capcom.com>
2026-02-26 19:14:06 +0900

    配置パスの変更

--- fakeControllApp/cyber-app/vite.config.js
+++ fakeControllApp/cyber-app/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // ブラウザが JS や CSS を探しに行けるように、公開URLのルートを指定します
+  base: '/html_utils/fakeControllApp/cyber-app/dist/',
 })
