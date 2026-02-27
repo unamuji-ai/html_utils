66a8163c833da20535b2060bf11e351eea8c9224
koji-hasunuma <koji-hasunuma@capcom.com>
2026-02-27 18:09:28 +0900

    import漏れを修正

--- fakeControllApp/cyber-app/vite.config.js
+++ fakeControllApp/cyber-app/vite.config.js
@@ -1,5 +1,9 @@
-// fakeControllApp/cyber-app/vite.config.js
+// ↓ この 1行目、2行目が必須です！
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/html_utils/fakeControllApp/cyber-app/dist/', 
+  // リポジトリ名に合わせてパスを指定
+  base: '/html_utils/', 
 })
